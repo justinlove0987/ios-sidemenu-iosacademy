ef8f250750fda6f2520aecc6151e87e120eb3556
justin.tseng <justin.tseng@108-BCA012.local>
2022-02-21 17:19:47 +0800

    first commit

---
+++ SideMenu/controller/ContainerViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SideMenu
+//
+//  Created by justin.tseng on 2022/2/21.
+//
+
+import UIKit
+
+class ContainerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
